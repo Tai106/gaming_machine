a544ed28815cca3e2963a3b6c96518defb4db6e0
Gravity <tevoned@gmail.com>
2023-01-22 19:35:34 -0500

    symbol counts

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import random
+
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "b": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.item():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
 
 
 # deposit function will be used for collecting user input.
@@ -15,9 +37,11 @@ def deposit():
             print("Please aenter a number.")
     return amount
 
+
 def get_number_of_lines():
-     while True:
-        lines = input("Enter the numbe rof lines to bet on (1-" +str(MAX_LINES) + ")? ")
+    while True:
+        lines = input(
+            "Enter the numbe of lines to bet on (1-" + str(MAX_LINES) + ")? ")
         if lines.isdigit():
             lines = int(lines)
             if 1 <= lines <= MAX_LINES:
@@ -26,10 +50,37 @@ def get_number_of_lines():
                 print("Enter a valid number of lines.")
         else:
             print("Please enter a number.")
-     return lines
+    return lines
+
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet?")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET < + amount <= MAX_BET:
+                break
+            else:
+                print(F"Amount must be between ${MIN_BET} - %{MAX_BET}.")
+        else:
+            print("Please aenter a number.")
+    return amount
+
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 
 main()
