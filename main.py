b65dcfc69b5f374941e3744bbc7c0b4be1385561
Gravity <tevoned@gmail.com>
2023-02-13 15:40:11 -0500

    spin feature

--- main.py
+++ main.py
@@ -117,8 +117,7 @@ def get_bet():
     return amount
 
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -137,6 +136,19 @@ def main():
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
     print(f"You won on", *winning_lines)
+    return winnings - total_bet
+
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press inter to play (q to quit).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print("You left with ${balance}")
 
 
 main()
